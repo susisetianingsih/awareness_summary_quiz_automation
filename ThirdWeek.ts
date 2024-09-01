41c9f2ce4434018cb900e6e0ff091a19f00c0d7e
susisetianingsih <93315332+susisetianingsih@users.noreply.github.com>
2024-09-01 19:46:43 +0700

    Update ThirdWeek.ts

--- ThirdWeek.ts
+++ ThirdWeek.ts
@@ -1,6 +1,6 @@
-function sendReminderEmailGRC() {
+function sendReminderEmailThirdWeek() {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var quizSheet = ss.getSheetByName("response"); // Replace 'Sheet1' with the appropriate sheet name
+  var quizSheet = ss.getSheetByName("Form Responses 1"); // Replace 'Sheet1' with the appropriate sheet name
 
   // Data from quiz sheet
   var quizData = quizSheet.getDataRange().getValues();
@@ -31,30 +31,19 @@ function sendReminderEmailGRC() {
   // Departement data
   var halodocDepartementData = departementData(employeeData);
 
+  // Chief data
+  var chiefEmailData = chiefData(employeeData);
+
   // Send email
   var sendEmail = sendAutomaticEmailGRC(
     employeeNotCompleteQuiz,
     employeeCompleteQuiz,
     employeeInsufficientValue,
     employeeSufficientValue,
-    halodocDepartementData
+    halodocDepartementData,
+    chiefEmailData
   );
   Logger.log(sendEmail); // Email info sent or not
-}
-
-function departementData(employeeData) {
-  // Save departement dictionary
-  var departementData = new Set();
-
-  for (var i = 1; i < employeeData.length; i++) {
-    const nameDepartemen = employeeData[i][2];
-
-    // Add the department name to the Set
-    departementData.add(nameDepartemen);
-  }
-
-  // Convert Set to Array
-  return [...departementData];
 }
 
 function sendAutomaticEmailGRC(
@@ -62,7 +51,8 @@ function sendAutomaticEmailGRC(
   employeeCompleteQuiz,
   employeeInsufficientValue,
   employeeSufficientValue,
-  halodocDepartementData
+  halodocDepartementData,
+  chiefEmailData
 ) {
   // Calculate the percentage of employees who have not completed and completed the quiz
   var notComplete =
@@ -99,23 +89,21 @@ function sendAutomaticEmailGRC(
             max-width: 600px;
             margin: 20px auto;
             padding: 20px;
+            border: solid 1px #9370DB;
             background-color: #fff;
-            border-radius: 5px;
-            border: solid 1px #E0004D;
-            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            float: left;
           }
           h1 {
-            font-size: 24px;
+            font-size: 16px;
             margin-bottom: 20px;
           }
-          h2 {
-            font-size: 20px;
-            margin-bottom: 20px;
-          }
-          p {
+          p, td {
             color: #666;
             font-size: 16px;
             margin-bottom: 10px;
+            text-align: justify;
           }
           table {
             width: 100%;
@@ -123,12 +111,15 @@ function sendAutomaticEmailGRC(
             margin-bottom: 20px;
           }
           th, td {
-            border: 1px solid #E0004D;
+            border: 1px solid #9370DB;
             padding: 8px;
             text-align: left;
           }
           th {
-            background-color: rgba(128, 0, 128, 0.1);
+            color: #9370DB;
+            font-size: 16px;
+            margin-bottom: 10px;
+            background-color: #E6E6FA;
           }
           .persentase {
             width: 50%;
@@ -137,30 +128,29 @@ function sendAutomaticEmailGRC(
       </head>
       <body>
         <div class="container">
-          <h1>Dear Bapak Mohammad Nasir,</h1>
-          <p>Here is a summary of the Cyber Security Knowledge Quiz results:</p>
-          <h2>✍️ 
-          Employe quiz complention status</h2>
+          <h1>Dear All,</h1>
+          <p>With regards to the information security and data privacy awareness program Q2 2024 edition. Herewith, we would like to provide you with the final result of the completion status on the awareness program with the topic of “Social Engineering Attack” and “8 Principles of Data Protection” in each department, as follow:</p>
+          <h1>Halosquads participation rate:</h1>
           <table>
-            <tr><th class="persentase">Completed</th><td class="persentase">${complete.toFixed(
+            <tr><th class="persentase">Participated</th><td class="persentase">${complete.toFixed(
               2
             )}%</td></tr>
-            <tr><th class="persentase">Not Completed</th><td class="persentase">${notComplete.toFixed(
+            <tr><th class="persentase">Not Participated</th><td class="persentase">${notComplete.toFixed(
               2
             )}%</td></tr>
           </table>
-          <h2>🌟 Employe quiz score status</h2>
+          <h1>Halosquads quiz status:</h1>
           <table>
-            <tr><th class="persentase">Sufficient</th><td class="persentase">${sufficient.toFixed(
+            <tr><th class="persentase">Pass</th><td class="persentase">${sufficient.toFixed(
               2
             )}%</td></tr>
-            <tr><th class="persentase">Insufficient</th><td class="persentase">${insufficient.toFixed(
+            <tr><th class="persentase">Failed</th><td class="persentase">${insufficient.toFixed(
               2
             )}%</td></tr>
           </table>
           <p>Here is the breakdown by department:</p>
           <table>
-            <tr><th>Department Name</th><th>Not Completed</th><th>Completed</th><th>Fail</th><th>Pass</th></tr>
+            <tr><th>Department Name</th><th>Participated</th><th>Not Participated</th><th>Pass</th><th>Failed</th></tr>
   `;
 
   var y = 0;
@@ -214,7 +204,7 @@ function sendAutomaticEmailGRC(
         (departementInsufficientData.length +
           departementSufficientData.length)) *
       100;
-    var employeSufficient =
+    var employeeSufficient =
       (departementSufficientData.length /
         (departementInsufficientData.length +
           departementSufficientData.length)) *
@@ -222,27 +212,34 @@ function sendAutomaticEmailGRC(
 
     // Write in the body of the email
     y += 1;
-    emailBody += `<tr><td>${y}. ${
-      halodocDepartementData[data]
-    }</td><td>${employeeNotComplete.toFixed(
-      2
-    )}%</td><td>${employeeComplete.toFixed(
-      2
-    )}%</td><td>${employeeInsufficient.toFixed(
-      2
-    )}%</td><td>${employeSufficient.toFixed(2)}%</td></tr>`;
+    emailBody += (
+      <tr>
+        <td>
+          ${y}. ${halodocDepartementData[data]}
+        </td>
+        <td>${employeeComplete.toFixed(2)}%</td>
+        <td>${employeeNotComplete.toFixed(2)}%</td>
+        <td>${employeeSufficient.toFixed(2)}%</td>
+        <td>${employeeInsufficient.toFixed(2)}%</td>
+      </tr>
+    );
   }
 
   emailBody += `
           </table>
-          <p>May the results of this quiz serve as a reference for improving the quality of employees' knowledge about Halodoc's cybersecurity.</p>
+          <p>May the results could serve as a reference for improving the employees' knowledge about Halodoc's cybersecurity awareness.</p>
+          <p>Thank you for your support in this program and see you again in the next awareness session!</p>
+          <p>Best regards,<br>ISDP - Security GRC and Data Privacy Team</p>
         </div>
       </body>
     </html>`;
 
+  var chiefEmailList = Object.values(chiefEmailData);
+  var ccEmail = ["email1@gmail.com", "email2@gmail.com"];
   MailApp.sendEmail({
-    to: "susisetia542@gmail.com",
-    subject: "Cyber Security Knowledge Quiz Results [Edisi I]",
+    to: chiefEmailList.join(","),
+    cc: ccEmail.join(","),
+    subject: "3rd Week Result - Q2 2024",
     htmlBody: emailBody,
   });
   return "Email sent successfully";
